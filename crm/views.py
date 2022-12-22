20d826913d2ea3173d7dc905534f6728bf531eda
Sashamxz <sasha.oom7@gmail.com>
2022-12-22 15:02:47 +0200

    changed crm views and templates

--- crm/views.py
+++ crm/views.py
@@ -98,7 +98,7 @@ class ShowProducts(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title=context['products'])
+        c_def = self.get_user_context(title='products')
         return dict(list(context.items()) + list(c_def.items()))
 
     # def get_queryset(self):
@@ -145,7 +145,7 @@ class ShowOrders(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title=context['orders'])
+        c_def = self.get_user_context(title='orders')
         return dict(list(context.items()) + list(c_def.items()))
     
 
